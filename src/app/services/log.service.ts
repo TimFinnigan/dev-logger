4629473966e31fdfed9f3081f876ad9bf3fc75c3
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-15 14:19:20 -0400

    remove default logs

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -16,9 +16,9 @@ export class LogService {
 
   constructor() {
     this.logs = [
-      { id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:32') },
-      { id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:34:02') },
-      { id: '3', text: 'Added logs component', date: new Date('12/27/2017 1:55:52') },
+      // { id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:32') },
+      // { id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:34:02') },
+      // { id: '3', text: 'Added logs component', date: new Date('12/27/2017 1:55:52') },
     ]
   }
 
