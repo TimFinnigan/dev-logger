459fd0de5ef3db442109e44ba8b4f107c2ed090f
Tim Finnigan <tim.finnigan1@gmail.com>
2020-06-15 10:13:05 -0400

    update log service to use of()

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -13,7 +13,7 @@ export class LogsComponent implements OnInit {
   constructor(private logService: LogService) { }
 
   ngOnInit(): void {
-    this.logs = this.logService.getLogs();
+    this.logService.getLogs().subscribe(logs => { this.logs = logs });
   }
 
   onSelect(log: Log) {
